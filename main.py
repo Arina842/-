4f232af6531ae017f3c0217e4850c872bc635efc
Arisha <79106539+Arina842@users.noreply.github.com>
2023-11-03 16:39:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(0, 4 * np.pi, 50)
+x = np.linspace(0, 2 * np.pi, 50)
 y = np.cos(x)
+y2 = np.sin(x)
+plt.axis([0,2 * np.pi,-1.1,1.1])
 plt.xlabel('Ось х')
 plt.ylabel('Ось y')
-plt.title('COS X')
+plt.title('Graf')
 plt.plot(x, y, color='pink', marker='o')
 plt.plot(x, y)
+plt.plot(x, y2, color='pink', marker='o')
+plt.plot(x, y2)
+plt.grid(True)
 plt.show()
 
