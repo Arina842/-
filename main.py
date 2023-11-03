5d111ceba12282e3a5e6b4f77a9f32f4e4947ada
Arisha <79106539+Arina842@users.noreply.github.com>
2023-11-03 16:55:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ import numpy as np
 
 x = np.linspace(0, 4 * np.pi, 50)
 y = np.cos(x)
+y2 = np.sin(x)
 plt.xlabel('Ось х')
 plt.ylabel('Ось y')
 plt.title('COS X')
 plt.plot(x, y, color='pink', marker='o')
 plt.plot(x, y)
+plt.plot(x, y2)
 plt.show()
 
+x_list = list(range(0, 5))
+y_list = [22, 17, 6, 9]
+plt.plot(x_list, y_list)
+plt.show()
